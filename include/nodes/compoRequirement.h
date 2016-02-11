69def11d9d509a73317ae2078faa28907d08c314
Tomáš Licek <licektom@fit.cvut.cz>
2016-02-11 17:30:53 +0100

    compoRequirement: Requirement node completed.

--- include/nodes/compoRequirement.h
+++ include/nodes/compoRequirement.h
@@ -1,8 +1,17 @@
 #pragma once
 
 #include "compoNode.h"
+#include "compoPort.h"
 
 class CCompoRequirement : public CCompoNode {
 private:
-    
+            bool                        m_externally;
+            std::vector<CCompoPort*>    m_ports;
+            
+public:
+                                        CCompoRequirement   (bool m_externally, std::vector<CCompoPort*> ports);
+    virtual                             ~CCompoRequirement  ();
+    virtual void                        print               (std::ostream& os) const;
+            bool                        isExternal          ();
+            std::vector<CCompoPort*> *  getPorts            ();
 };
---
+++ src/nodes/compoRequirement.cpp
@@ -0,0 +1,39 @@
+#include "compoRequirement.h"
+
+CCompoRequirement::CCompoRequirement(bool externally = false, std::vector<CCompoPort*> ports = std::vector<CCompoPort*>(0))
+: CCompoNode(NodeTypeEnum::PROVISION), m_externally(externally), m_ports(ports)
+{}
+
+CCompoRequirement::~CCompoRequirement() {
+    for (CCompoPort *port : m_ports) {
+        delete port;
+    }
+}
+
+void CCompoRequirement::print(std::ostream& os) const {
+    if (!m_toString) {
+        os << typeName(m_type) << " ";
+    }
+    os << "\t";
+    if (m_externally) {
+        os << "externally ";
+    }
+
+    os << "provides {" << std::endl;
+    
+    for (CCompoPort *port : m_ports) {
+        os << "\t";
+        os << *port << std::endl;
+    }
+    
+    os << "\t";
+    os << "}" << std::endl;
+}
+
+bool CCompoRequirement::isExternal() {
+    return m_externally;
+}
+
+std::vector<CCompoPort*> * CCompoRequirement::getPorts() {
+    return &m_ports;
+}
