a2b182509a4f347dc2e5d94e8788165ec48e9a6f
Tomáš Licek <licektom@fit.cvut.cz>
2016-02-12 17:12:48 +0100

    compoService: Service node.

--- include/nodes/compoService.h
+++ include/nodes/compoService.h
@@ -1,22 +1,10 @@
 #pragma once
 
-#include "compoNode.h"
-#include "compoSymbol.h"
+#include "compoAbstractService.h"
 
-
-class CCompoService : public CCompoNode {
-private:
-            CCompoSymbol              * m_name;
-            std::vector<CCompoSymbol*>  m_params;
-            std::vector<CCompoNode*>    m_body;
-            
+class CCompoService : public CCompoAbstractService {
 public:
-                                        CCompoService       (CCompoSymbol *name, std::vector<CCompoSymbol*> params, std::vector<CCompoNode*> body);
-    virtual                             ~CCompoService      ();
-    virtual void                        print               (std::ostream& os) const;
-            CCompoSymbol *              getName             () const;
-            std::vector<CCompoNode*> *  getBody             ();
-            void                        setBody             (std::vector<CCompoNode*> body);
-            std::vector<CCompoSymbol*> *getParams           ();
-            void                        setParam            (CCompoSymbol *param);
+
+                CCompoService   (CCompoSymbol *name, std::vector<CCompoSymbol*> params, std::vector<CCompoNode*> body);
+    virtual     ~CCompoService  ();
 };
--- src/nodes/compoService.cpp
+++ src/nodes/compoService.cpp
@@ -1,57 +1,7 @@
 #include "compoService.h"
 
 CCompoService::CCompoService(CCompoSymbol* name = nullptr, std::vector<CCompoSymbol*> params = std::vector<CCompoSymbol*>(0), std::vector<CCompoNode*> body = std::vector<CCompoNode*>(0))
-: CCompoNode(NodeTypeEnum::SERVICE), m_name(name), m_params(params), m_body(body)
-{}
+:   CCompoNode(NodeTypeEnum::SERVICE),
+    CCompoAbstractService(name, params, body) {}
 
-CCompoService::~CCompoService() {
-    delete m_name;
-    
-    for (CCompoSymbol *symbol : m_params) {
-        delete symbol;
-    }
-    
-    for (CCompoNode *expr : m_body) {
-        delete expr;
-    }
-}
-
-void CCompoService::print(std::ostream& os) const {
-    os << "\t";
-    os << typeName(m_type) << " ";
-    
-    os << *m_name << " (";
-    
-    bool first = true;
-    for (CCompoSymbol *symbol : m_params) {
-        if (!first) {
-            os << ", ";
-            first = false;
-        }
-        os << *symbol;
-    }
-    
-    os << ") {" << std::endl;
-    os << "\t";
-    os << "}" << std::endl;
-}
-
-CCompoSymbol * CCompoService::getName() const {
-    return m_name;
-}
-
-std::vector<CCompoNode*> * CCompoService::getBody() {
-    return &m_body;
-}
-
-void CCompoService::setBody(std::vector<CCompoNode*> body) {
-    m_body = body;
-}
-
-std::vector<CCompoSymbol*> * CCompoService::getParams() {
-    return &m_params;
-}
-
-void CCompoService::setParam(CCompoSymbol* param) {
-    m_params.push_back(param);
-}
+CCompoService::~CCompoService() {}
