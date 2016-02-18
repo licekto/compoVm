48f5c19cad6415c6bb27519661657654c689db3c
Tomáš Licek <licektom@fit.cvut.cz>
2016-02-18 18:05:23 +0100

    CMakeList+main: Little changes.

--- src/main.cpp
+++ src/main.cpp
@@ -18,17 +18,23 @@ int main(int argc, char **argv) {
     Lexer lexer;
     ParserWrapper parser(&lexer);
 
+    parser.parseAll(infile);
+    
+    /*
     while (1) {
         if (!parser.parse(infile)) {
             break;
         }
     }
+    */
 
     infile.close();
     
     for (unsigned int i = 0; i < parser.getRootNodesSize(); ++i) {
         std::cout << *parser.getRootNodeAt(i) << std::endl;
     }
+    
+    parser.clear();
 
     return 0;
 }
