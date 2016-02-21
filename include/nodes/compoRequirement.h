7f1ae072a51c6f9a5cf031ebdeecd758b4258a0d
Tomáš Licek <licektom@fit.cvut.cz>
2016-02-22 00:54:16 +0100

    compoRequirement: Rule of five.

--- include/nodes/compoRequirement.h
+++ include/nodes/compoRequirement.h
@@ -15,12 +15,38 @@ namespace compo {
                                                 * @param type: Type of visibility
                                                 * @param ports: Vector of ports
                                                 */
-                                                CCompoRequirement     ( visibilityType type = visibilityType::EXTERNAL,
-                                                                        const std::vector<CCompoPort*>& ports = std::vector<CCompoPort*>(0));
+                                                CCompoRequirement       ( visibilityType type = visibilityType::EXTERNAL,
+                                                                          const std::vector<CCompoPort*>& ports = std::vector<CCompoPort*>(0) );
+                                                /**
+                                                * Copy constructor
+                                                * @param other: reference to another object of same type
+                                                */
+                                                CCompoRequirement       (const CCompoRequirement& other);
+                                                
+                                                /**
+                                                * Move constructor
+                                                * @param other: rvalue-reference to another object of same type
+                                                */
+                                                CCompoRequirement       (CCompoRequirement&& other) noexcept;
+                                                
+                                                /**
+                                                * Copy assignment operator
+                                                * @param other: reference to another object of same type
+                                                * @return reference to assigned object
+                                                */
+                CCompoRequirement&              operator=               (const CCompoRequirement& other);
+                                                
+                                                /**
+                                                * Move assignment operator
+                                                * @param other: rvalue-reference to another object of same type
+                                                * @return reference to assigned object
+                                                */
+                CCompoRequirement&              operator=               (CCompoRequirement&& other) noexcept;
+                                                
                                                 /**
                                                 * Virtual destructor
                                                 */
-        virtual                                 ~CCompoRequirement    ();
+        virtual                                 ~CCompoRequirement      ();
     };
 
 }
--- src/nodes/compoRequirement.cpp
+++ src/nodes/compoRequirement.cpp
@@ -7,7 +7,32 @@ namespace compo {
     :   CCompoNode(NodeTypeEnum::REQUIREMENT),
         CCompoAbstractReqProv(type, ports)
     {}
+    
+    CCompoRequirement::CCompoRequirement(const CCompoRequirement& other)
+    : CCompoNode(req.m_nodeType),
+      CCompoAbstractReqProv(req)
+    {}
 
+    
+    CCompoRequirement::CCompoRequirement(CCompoRequirement&& other) noexcept
+    : CCompoNode(std::move(req.m_nodeType)),
+      CCompoAbstractReqProv(std::move(req))
+    {}
+    
+    CCompoRequirement& CCompoRequirement::operator =(const CCompoRequirement& other) {
+        if (&req != this) {
+            *this = req;
+        }
+        return *this;
+    }
+
+    CCompoRequirement& CCompoRequirement::operator =(CCompoRequirement&& other) noexcept {
+        if (&req != this) {
+            *this = std::move(req);
+        }
+        return *this;
+    }
+    
     CCompoRequirement::~CCompoRequirement() {
     }
 
