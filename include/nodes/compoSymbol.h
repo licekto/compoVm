bbddaec6284388cfae52e285682d0f8805b5301c
Tomáš Licek <licektom@fit.cvut.cz>
2016-02-22 01:28:25 +0100

    compoSymbol: Rule of five draft (although it's not necessary).

--- include/nodes/compoSymbol.h
+++ include/nodes/compoSymbol.h
@@ -20,6 +20,32 @@ namespace compo {
                                                 CCompoSymbol        (const std::string& name);
                                                 
                                                 /**
+                                                * Copy constructor
+                                                * @param other: reference to another object of same type
+                                                */
+                                                //CCompoSymbol        (const CCompoSymbol& other);
+                                                
+                                                /**
+                                                * Move constructor
+                                                * @param other: rvalue-reference to another object of same type
+                                                */
+                                                //CCompoSymbol        (CCompoSymbol&& other) noexcept;
+                                                
+                                                /**
+                                                * Copy assignment operator
+                                                * @param other: reference to another object of same type
+                                                * @return reference to assigned object
+                                                */
+                //CCompoSymbol&                   operator=           (const CCompoSymbol& other);
+                
+                                                /**
+                                                * Move assignment operator
+                                                * @param other: rvalue-reference to another object of same type
+                                                * @return reference to assigned object
+                                                */
+                //CCompoSymbol&                   operator=           (CCompoSymbol&& other) noexcept;
+                                                
+                                                /**
                                                 * Virtual destructor
                                                 */
         virtual                                 ~CCompoSymbol       ();
--- src/nodes/compoSymbol.cpp
+++ src/nodes/compoSymbol.cpp
@@ -6,7 +6,36 @@ namespace compo {
     :   CCompoNode(NodeTypeEnum::SYMBOL),
         m_name(name)
     {}
+    /*
+    CCompoSymbol::CCompoSymbol(const CCompoSymbol& other)
+    : CCompoNode(other.m_nodeType),
+      m_name(other.m_name)
+    {}
     
+    CCompoSymbol::CCompoSymbol(CCompoSymbol&& other) noexcept
+    : CCompoNode(std::move(other.m_nodeType)),
+      m_name(std::move(other.m_name))
+    {
+        other.m_name.clear();
+    }
+    
+    CCompoSymbol& CCompoSymbol::operator= (const CCompoSymbol& other) {
+         if (&other != this) {
+            this->m_nodeType = other.m_nodeType;
+            this->m_name = other.m_name;
+        }
+        return *this;
+    }
+    
+    CCompoSymbol& CCompoSymbol::operator= (CCompoSymbol&& other) noexcept {
+         if (&other != this) {
+            this->m_nodeType = std::move(other.m_nodeType);
+            this->m_name = std::move(other.m_name);
+            other.m_name.clear();
+        }
+        return *this;
+    }
+    */
     CCompoSymbol::~CCompoSymbol() {
 
     }
