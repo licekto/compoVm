4d70c9d7c12951d8ba917d8eaa672124b58e4083
Tomáš Licek <licektom@fit.cvut.cz>
2016-03-02 18:12:33 +0100

    whileStatement: Refactoring.

--- include/nodes/procedural/whileStatement.h
+++ include/nodes/procedural/whileStatement.h
@@ -16,8 +16,8 @@ namespace nodes {
 		 */
 		class CWhileStatement : public CAbstractStatement {
 		  private:
-			std::shared_ptr<nodes::procedural::CAbstractExpression>    m_condition;           /**< Start condition node */
-			std::shared_ptr<nodes::procedural::CCompoundBody>           m_body;                 /**< Body */
+			std::shared_ptr<nodes::procedural::CAbstractExpression> m_condition; /**< Start condition node */
+			std::shared_ptr<nodes::procedural::CCompoundBody> m_body; /**< Body */
 
 		  public:
 			/**
@@ -27,27 +27,27 @@ namespace nodes {
 			* @param step: Step node pointer
 			* @param body: Reference to vector of body nodes
 			*/
-			CWhileStatement                ( std::shared_ptr<nodes::procedural::CAbstractExpression> cond = nullptr,
-			                                 std::shared_ptr<nodes::procedural::CCompoundBody> body = nullptr);
+			CWhileStatement(std::shared_ptr<nodes::procedural::CAbstractExpression> cond = nullptr,
+                                        std::shared_ptr<nodes::procedural::CCompoundBody> body = nullptr);
 
 			/**
 			* Virtual print function to call from operator <<
 			* @param os: output stream
 			* @see operator <<()
 			*/
-			virtual void                            print               (std::ostream& os) const;
+			virtual void print(std::ostream& os) const;
 
 			/**
 			* Body getter
 			* @return Constant pointer to body
 			*/
-			std::shared_ptr<nodes::procedural::CCompoundBody>                    getBody () const;
+			std::shared_ptr<nodes::procedural::CCompoundBody> getBody() const;
 
 			/**
 			* Condition getter
 			* @return Pointer to condition node
 			*/
-			std::shared_ptr<nodes::procedural::CAbstractExpression>                    getCondition        () const;
+			std::shared_ptr<nodes::procedural::CAbstractExpression> getCondition() const;
 		};
 
 	}
