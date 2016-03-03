e589fd09612163fc42f27f7d386053437cf6a32d
Tomáš Licek <licektom@fit.cvut.cz>
2016-03-03 20:06:29 +0100

    CMakeLists + teststatsdoc.pl: Support for code coverage report (see wiki).

--- script/teststatsdoc.pl
+++ script/teststatsdoc.pl
@@ -11,6 +11,12 @@ my $testCommand = "ctest -T Test -D ExperimentalBuild -D ExperimentalTest -D Exp
 my $oclintCommand = "oclint-json-compilation-database -e ../src/generated/parser.cpp -e ../src/generated/lexer.cpp -- -rc=LONG_LINE=200 -report-type html -o oclint.html";
 my $doxygenCommand = "doxygen Doxyfile";
 my $cppcheckCommand = "cppcheck -j 4 -i src/generated/ -i include/generated/ src/ include/ --force --enable=warning,performance,information,style --xml 2> cppcheck.xml";
+my $lcovCommand1 = "lcov --capture --directory ../ -o coverage.info";
+my $lcovCommand2 = "lcov --remove coverage.info \"/usr*\" -o coverage.info";
+my $lcovCommand3 = "lcov --remove coverage.info \"test/*\" -o coverage.info";
+my $lcovCommand4 = "lcov --remove coverage.info \"include/generated/*\" -o coverage.info";
+my $lcovCommand5 = "lcov --remove coverage.info \"src/generated/*\" -o coverage.info";
+my $genhtmlCommand = "genhtml coverage.info --output-directory ../doc/coverage";
 
 chdir("../resources");
 say "Running Doxygen...";
@@ -22,6 +28,17 @@ chdir("../build/");
 say "Running tests...";
 system($testCommand);
 say "Tests completed";
+say "-----------------------------------\n";
+
+say "Running lcov...";
+system($lcovCommand1);
+system($lcovCommand2);
+system($lcovCommand3);
+system($lcovCommand4);
+system($lcovCommand5);
+system($genhtmlCommand);
+system("rm ../coverage.info");
+say "Code coverage report completed";
 say "-----------------------------------\n";
 
 my $oclintFile = "compile_commands.json";
