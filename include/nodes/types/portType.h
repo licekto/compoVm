7c88d92fce27f649d9dda6f862d1761c4883a509
Tomáš Licek <licektom@fit.cvut.cz>
2016-03-03 20:07:45 +0100

    portType.h: Types of ports.

---
+++ include/nodes/types/portType.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace nodes {
+
+	namespace types {
+
+		/**
+		 * \enum portType
+		 * \brief Types of ports
+		 */
+		enum portType {
+			EXPLICIT_LIST,
+			UNIVERSAL,
+			NAMED,
+		};
+
+	}
+
+}
