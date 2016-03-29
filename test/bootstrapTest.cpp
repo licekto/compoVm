86ce0e601476ce965ccf13ed8654fc6eb91f05b5
Tomáš Licek <licektom@fit.cvut.cz>
2016-03-29 21:40:04 +0200

    Bootstrap.

--- test/bootstrapTest.cpp
+++ test/bootstrapTest.cpp
@@ -20,28 +20,21 @@ BOOST_AUTO_TEST_SUITE(bootstrapTest)
 
 BOOST_AUTO_TEST_CASE(basicTest) {
     
-    ptr(interpreter::core::CBootstrap) bootstrap =
-            new_ptr(interpreter::core::CBootstrap)                              // new Boostrap
-               (new_ptr(interpreter::core::CCoreModules)                        // new Core Modules
-                   (new_ptr(ParserWrapper)                                      // new Parser
-                       (new_ptr(Lexer)(),                                       // new Lexer
-                        new_ptr(ast::semantic::CGlobalDescriptorTable)())),     // new Descriptor table
-                new_ptr(interpreter::core::CInterpreter)(parser, coreModules)); // new Interpreter
+    ptr(ParserWrapper) parser = new_ptr(ParserWrapper)(new_ptr(Lexer)(), new_ptr(ast::semantic::CGlobalDescriptorTable)());
+    ptr(interpreter::core::CCoreModules) coreModules = new_ptr(interpreter::core::CCoreModules)(parser);
+    ptr(interpreter::core::CInterpreter) interpreter = new_ptr(interpreter::core::CInterpreter)(parser, coreModules);
+    ptr(interpreter::core::CBootstrap) bootstrap = new_ptr(interpreter::core::CBootstrap)(coreModules, interpreter);
     
-    // run bootstrapping
     bootstrap->boostrap();
     
-    // get Descriptor component
     ptr(interpreter::memory::objects::CComponent) descriptor = bootstrap->getCoreComponent(interpreter::core::coreModuleType::DESCRIPTOR);
     
-    // check Descriptor component
     BOOST_CHECK_EQUAL(descriptor->getNumberOfPorts(), 4);
     BOOST_CHECK_EQUAL(descriptor->getPortAt(0)->m_port->getPrimitivePortByName("name")->getName(), "name");
     BOOST_CHECK_EQUAL(descriptor->getPortAt(0)->m_type, interpreter::memory::objects::portType::REQUIREMENT);
     BOOST_CHECK_EQUAL(descriptor->getPortAt(0)->m_visibility, interpreter::memory::objects::portVisibility::INTERNAL);
     ptr(interpreter::memory::objects::primitives::CPrimitivePort) primitivePort = cast(interpreter::memory::objects::primitives::CPrimitivePort)
             (descriptor->getPortAt(0)->m_port->getPrimitivePortByName("name"));
-    
 }
 
 BOOST_AUTO_TEST_SUITE_END()
