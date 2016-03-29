c68362309042ec6b5b0e4f4464b2f3224127888e
Tomáš Licek <licektom@fit.cvut.cz>
2016-03-29 21:41:21 +0200

    Bootstrap.

--- test/bootstrapTest.cpp
+++ test/bootstrapTest.cpp
@@ -19,16 +19,19 @@
 BOOST_AUTO_TEST_SUITE(bootstrapTest)
 
 BOOST_AUTO_TEST_CASE(basicTest) {
-    
+    // Crete parser, core modules, interpreter and bootstrap
     ptr(ParserWrapper) parser = new_ptr(ParserWrapper)(new_ptr(Lexer)(), new_ptr(ast::semantic::CGlobalDescriptorTable)());
     ptr(interpreter::core::CCoreModules) coreModules = new_ptr(interpreter::core::CCoreModules)(parser);
     ptr(interpreter::core::CInterpreter) interpreter = new_ptr(interpreter::core::CInterpreter)(parser, coreModules);
     ptr(interpreter::core::CBootstrap) bootstrap = new_ptr(interpreter::core::CBootstrap)(coreModules, interpreter);
     
+    // Bootstrap
     bootstrap->boostrap();
     
+    // Get Descriptor component
     ptr(interpreter::memory::objects::CComponent) descriptor = bootstrap->getCoreComponent(interpreter::core::coreModuleType::DESCRIPTOR);
     
+    // Check Descriptor component
     BOOST_CHECK_EQUAL(descriptor->getNumberOfPorts(), 4);
     BOOST_CHECK_EQUAL(descriptor->getPortAt(0)->m_port->getPrimitivePortByName("name")->getName(), "name");
     BOOST_CHECK_EQUAL(descriptor->getPortAt(0)->m_type, interpreter::memory::objects::portType::REQUIREMENT);
