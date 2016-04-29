968aaa224a52280bf11a8fd67ea48574e25bf5f4
tomas <tomas@tomas-arch-vm>
2016-04-29 20:35:39 +0200

    Main

--- src/main.cpp
+++ src/main.cpp
@@ -7,31 +7,31 @@
 #include "definitions/interpreterDefinitions.h"
 
 std::string readFile(const std::string& filename) {
-    	std::ifstream infile(filename);
-        if (!infile.good()) {
-            throw exceptions::runtime::CWrongParametersException("File '" + filename + "' does not exist!");
-        }
+	std::ifstream infile(filename);
+	if (!infile.good()) {
+		throw exceptions::runtime::CWrongParametersException("File '" + filename + "' does not exist!");
+	}
 
-        std::string line, code;
-        while (std::getline(infile, line)) {
-            code += line;
-        }
+	std::string line, code;
+	while (std::getline(infile, line)) {
+		code += line;
+	}
 	infile.close();
-        
-        return code;
+
+	return code;
 }
 
 ptr(core_interpreter) init() {
-        ptr(ParserWrapper) parser = new_ptr(ParserWrapper)(new_ptr(Lexer)(), new_ptr(ast::semantic::CSyntaxDescriptorTable)());
-        ptr(interpreter::memory::memspace::CDescriptorTable) table = new_ptr(interpreter::memory::memspace::CDescriptorTable)();
-        ptr(interpreter::memory::memspace::CMemory) memory = new_ptr(interpreter::memory::memspace::CMemory)();
-        ptr(core_bootstrap1) bootstrap1 = new_ptr(core_bootstrap1)(new_ptr(interpreter::core::CCoreModules)(parser), nullptr, memory);
-        memory->setBootstrap1(bootstrap1);
-        ptr(core_bootstrap2) bootstrap2 = new_ptr(core_bootstrap2)(bootstrap1);
-        ptr(core_interpreter) interpreter = new_ptr(core_interpreter)(parser, bootstrap2, table);
-        bootstrap1->setInterpreter(interpreter);
-        
-        return interpreter;
+	ptr(ParserWrapper) parser = new_ptr(ParserWrapper)(new_ptr(Lexer)(), new_ptr(ast::semantic::CSyntaxDescriptorTable)());
+	ptr(interpreter::memory::memspace::CDescriptorTable) table = new_ptr(interpreter::memory::memspace::CDescriptorTable)();
+	ptr(interpreter::memory::memspace::CMemory) memory = new_ptr(interpreter::memory::memspace::CMemory)();
+	ptr(core_bootstrap1) bootstrap1 = new_ptr(core_bootstrap1)(new_ptr(interpreter::core::CCoreModules)(parser), nullptr, memory);
+	memory->setBootstrap1(bootstrap1);
+	ptr(core_bootstrap2) bootstrap2 = new_ptr(core_bootstrap2)(bootstrap1);
+	ptr(core_interpreter) interpreter = new_ptr(core_interpreter)(parser, bootstrap2, table);
+	bootstrap1->setInterpreter(interpreter);
+
+	return interpreter;
 }
 
 int main(int argc, char **argv) {
@@ -39,21 +39,20 @@ int main(int argc, char **argv) {
 		std::cerr << "Wrong arguments" << std::endl << "Usage: compoVm <file_name>" << std::endl;
 		return -1;
 	}
-        std::string code;
-        try {
-            code = readFile(std::string(argv[1]));
-        }
-        catch (const exceptions::runtime::CWrongParametersException& ex) {
-            std::cerr << ex.what() << std::endl;
-            return -1;
-        }
-        ptr(core_interpreter) interpreter = init();
-        try {
-            interpreter->run(code);
-        } catch (const std::exception& ex) {
-            std::cerr << ex.what() << std::endl;
-            return -1;
-        }
+	std::string code;
+	try {
+		code = readFile(std::string(argv[1]));
+	} catch (const exceptions::runtime::CWrongParametersException& ex) {
+		std::cerr << ex.what() << std::endl;
+		return -1;
+	}
+	ptr(core_interpreter) interpreter = init();
+	try {
+		interpreter->run(code);
+	} catch (const std::exception& ex) {
+		std::cerr << ex.what() << std::endl;
+		return -1;
+	}
 
 	return 0;
 }
